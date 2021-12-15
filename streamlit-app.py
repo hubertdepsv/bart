487420287fed535f4eaf03bf8b0ea042c4b66679
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-12-15 18:50:38 +0100

    Changed requirements and streamlit app

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-
-
-@st.cache
+from transformers import pipeline
 
 option = st.selectbox(
     "Select an Option",
-    ["Text Summarization", ]
+    [
+        "Text Summarization",
+    ],
 )
 
-
-
-if __name__ == "__main__":
-    # df = get_data()
-    # generate_dashboard(df)
+text = st.text_area(label="Enter text")
+if text:
+    classifier = pipeline("summarization")
+    answer = classifier(text, max_length=200, min_length=10)
+    st.write(answer)
