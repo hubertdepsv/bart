c5409148d5d68ca138addc5728b70bab6c501546
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-12-15 19:03:37 +0100

    Switched to Pierre's parameters BART model

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,6 +10,6 @@ option = st.selectbox(
 
 text = st.text_area(label="Enter text")
 if text:
-    classifier = pipeline("summarization")
-    answer = classifier(text, max_length=200, min_length=10)
-    st.write(answer)
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    answer = summarizer(text, max_length=130, min_length=30, do_sample=False)
+    st.write(answer[0]["summary"])
