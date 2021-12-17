d921d677f20ddd646178ac943433a7c99833d7e7
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-12-17 13:37:50 +0100

    Changed the layout of the online app

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,12 +8,16 @@ def generate_header():
 
 def summarize():
     text = st.text_area(label="Enter text")
-    if text:
+    submit = st.button("Summarize")
+
+    if submit:
         # use facebook's pre-trained model in case you don't have our model's weights locally
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        answer = summarizer(text, max_length=130, min_length=30, do_sample=False)
+        st.subheader("Summary")
+        with st.spinner(text="This might take a while..."):
+            summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+            answer = summarizer(text, max_length=130, min_length=30, do_sample=False)
         st.write(answer[0]["summary_text"])
-        st.write(
+        st.text(
             f'The summary includes {len(answer[0]["summary_text"].split())} tokens while the input text has {len(text.split())} tokens.'
         )
 
