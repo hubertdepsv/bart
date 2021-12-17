53390d302cc5db9a8f5d4853317bf4bf10b3947f
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-12-17 13:44:01 +0100

    Modified the local app to handle out-of-repo binary model and aligned layouts

--- streamlit-local.py
+++ streamlit-local.py
@@ -5,12 +5,11 @@ from summarizer import summarize_a_text
 
 def generate_header():
     st.title("Text summarization")
-    # st.
 
 
 def choose_model():
     option = st.selectbox(
-        "Select an Option",
+        "Select a model",
         ["Our model", "Facebook pre-trained model"],
     )
     return option
@@ -18,22 +17,29 @@ def choose_model():
 
 def summarize(option):
     text = st.text_area(label="Enter text")
-    if text:
+    submit = st.button("Summarize")
+    if submit:
         if option == "Our model":
             answer = summarize_a_text(
                 text,
-                path="model_files",
+                path="../model_files",
                 source_len=1024,
                 max_length_pred=128,
                 min_length_pred=30,
             )
             st.write(answer)
+            st.text(
+                f"The summary includes {len(answer.split())} tokens while the input text has {len(text.split())} tokens."
+            )
 
         elif option == "Facebook pre-trained model":
             # use facebook's pre-trained model in case you don't have our model's weights locally
             summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
             answer = summarizer(text, max_length=130, min_length=30, do_sample=False)
             st.write(answer[0]["summary_text"])
+            st.text(
+                f'The summary includes {len(answer[0]["summary_text"].split())} tokens while the input text has {len(text.split())} tokens.'
+            )
 
 
 if __name__ == "__main__":
